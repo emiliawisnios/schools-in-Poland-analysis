cfd7b29d296894a97cc3cf6dd8b292db9f7d24cf
Emilia Wiśnios <57295153+emiliawisnios@users.noreply.github.com>
2021-02-09 16:07:48 +0100

    Update calculate.py

--- school_analysis/calculate.py
+++ school_analysis/calculate.py
@@ -1,1 +1,34 @@
+import numpy as np
+import pandas as pd
 
+
+def students_per_teacher_minimum(school_dataframe):
+    school_dataframe.drop(school_dataframe[school_dataframe['Teachers'] == 0].index, inplace=True)
+    school_dataframe['Average_min'] = school_dataframe['Students']/school_dataframe['Teachers']
+    school_dataframe.replace(np.inf, -1, inplace=True)
+    school_dataframe.replace(np.nan, -1, inplace=True)
+    school_dataframe = school_dataframe.round(2)
+    districts_and_type = school_dataframe.groupby(['District', 'SchoolType'])['Average_min'].min()
+    return districts_and_type
+
+
+def students_per_teacher_maximum(school_dataframe):
+    school_dataframe.drop(school_dataframe[school_dataframe['Teachers'] == 0].index, inplace=True)
+    school_dataframe['Average_max'] = school_dataframe['Students']/school_dataframe['Teachers']
+    school_dataframe.replace(np.inf, -1, inplace=True)
+    school_dataframe.replace(np.nan, -1, inplace=True)
+    school_dataframe = school_dataframe.round(2)
+    districts_and_type = school_dataframe.groupby(['District', 'SchoolType'])['Average_max'].max()
+    return districts_and_type
+
+
+def students_per_teacher_average(school_dataframe):
+    school_dataframe.drop(school_dataframe[school_dataframe['Teachers'] == 0].index, inplace=True)
+    districts_and_type = school_dataframe.groupby(['District', 'SchoolType']).agg({'Students': 'sum',
+                                                                                       'Teachers': 'sum'})
+    districts_and_type['Average'] = districts_and_type['Students']/ districts_and_type['Teachers']
+    districts_and_type.replace(np.inf, -1, inplace=True)
+    districts_and_type.replace(np.nan, -1, inplace=True)
+    districts_and_type = districts_and_type.round(2)
+    districts_and_type_drop = districts_and_type.drop(['Students', 'Teachers'], axis=1)
+    return districts_and_type_drop
